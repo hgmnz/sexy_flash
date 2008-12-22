cfbcc5d56e48d487433cd70dd9bda5d0ae3d6165
Harold A. Gimenez <harold.gimenez@gmail.com>
2008-12-22 00:34:17 -0500

    Refactored CSS, added icons for flash messages

--- install.rb
+++ install.rb
@@ -1,3 +1,8 @@
 # Install hook code here
 puts 'Copying stylesheet'
 FileUtils.cp File.join(File.dirname(__FILE__), 'public/stylesheets/sexy_flash.css'), "#{RAILS_ROOT}/public/stylesheets/"
+puts 'Copying icons'
+destination = File.join(RAILS_ROOT, 'public/images/icons/sexy_flash')
+source = File.dirname(__FILE__) + '/public/images/icons/sexy_flash'
+FileUtils.mkdir(destination)
+FileUtils.cp_r(Dir.glob(source + '*.*'), destination)
